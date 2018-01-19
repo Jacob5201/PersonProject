be9ec6f723b450d563ff4e73a655e8f976f254ad
Jacob5201 <1365449906@qq.com>
2018-01-19 15:33:28 +0800

    tianji

---
+++ Test/src/pyj/com/Person.java
@@ -0,0 +1,35 @@
+package pyj.com;
+
+public class Person {
+	 public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	 String name;
+	
+	 String sex;
+	
+	 int age;
+	
+
+}
